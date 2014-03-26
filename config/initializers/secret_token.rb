28f79859fcda4db365a88e684d133e78a39030cb
Nicholas Major <nick@crowdengine.com>
2014-03-25 20:22:06 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Concept::Application.config.secret_key_base = '9d73224aca350fe8a573dc871427712d1a0c1b8d086f01b2eba0a7dc9b2ff9e80bcc317d3a8dcdb139962aa94b3fde3b213ed00820dac1103438859209d4e408'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Concept::Application.config.session_store :cookie_store, key: '_concept_session'
