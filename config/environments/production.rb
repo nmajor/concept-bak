3681195e8d79fcbeca83583e26f2f58bee2aac01
Nicholas Major <nick@crowdengine.com>
2014-04-16 23:40:33 -0600

    Setting action mailer domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,7 +81,7 @@ Concept::Application.configure do
     password: 'SuperHappy123'
   }
   # ActionMailer Config
-  config.action_mailer.default_url_options = { :host => 'example.com' }
+  config.action_mailer.default_url_options = { :host => 'serene-forest-1438.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
